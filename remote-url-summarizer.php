fd5487f4ca6bb5b1be516f365185feb1c710cd25
Jonathan Daggerhart <daggerhart@gmail.com>
2015-05-29 12:07:50 -0400

    fighting with case sensitivity and autoloading

--- remote-url-summarizer.php
+++ remote-url-summarizer.php
@@ -113,7 +113,7 @@ spl_autoload_register(function ($class) {
   // replace the namespace prefix with the base directory, replace namespace
   // separators with directory separators in the relative class name, append
   // with .php
-  $file = strtolower($base_dir) . str_replace('\\', '/', $relative_class) . '.php';
+  $file = strtolower($base_dir . str_replace('\\', '/', $relative_class) . '.php');
 
   // if the file exists, require it
   if (file_exists($file)) {
